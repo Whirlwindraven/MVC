bdd474fc900a1000da11bfe1ae65ff921148fb3d
Whirlwindraven <fadysherif1998@gmail.com>
2023-05-14 13:23:50 -0400

    populated home-routes.js file

---
+++ controllers/home-routes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const { Blog, User, Comment } = require('../models');
+const authMiddleware = require('../utils/authentication');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      include: [{ model: User, attributes: ['username'] }],
+    });
+
+    const blogs = blogData.map(blog => blog.get({ plain: true }));
+
+    res.render('homepage', { 
+      blogs, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        { model: User, attributes: ['username'] },
+        { model: Comment, include: [User] }
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render('blog', {
+      ...blog,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/profile', authMiddleware, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('signup');
+});
+
+module.exports = router;
