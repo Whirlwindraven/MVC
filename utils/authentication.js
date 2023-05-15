db62417ee7d7f1e2ad88a2eba91b5c511dacf126
Whirlwindraven <fadysherif1998@gmail.com>
2023-05-15 15:35:13 -0400

    fixed bug in authentication.js

--- utils/authentication.js
+++ utils/authentication.js
@@ -1,13 +1,12 @@
 const withAuth = (req, res, proceed) => {
     // This middleware checks if the user is authenticated by looking at the 'logged_in' property in the session object.
-    if (request.session.logged_in) {
+    if (req.session.logged_in) {
         // If the user is authenticated, the request can proceed to the next middleware or route handler.
         proceed();
     } else {
         // If the user is not authenticated, they are redirected to the login page.
-        response.redirect('/login');
+        res.redirect('/login');
     }
 };
 
-  
-  module.exports = withAuth;
+module.exports = withAuth;
